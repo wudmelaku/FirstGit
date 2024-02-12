2314e5473d208ab128cdbb56b7a7b80af8919e6b
wudmelaku <wudmelaku@ju2.edu.et>
2024-02-12 09:44:57 +0300

    first commit
    first

---
+++ Script1.js
@@ -0,0 +1,12 @@
+// scroll selection link.
+let sections = querySelectorAll('section');
+let navlinks = querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach{ }
+
+
+
+
+}
+
+
